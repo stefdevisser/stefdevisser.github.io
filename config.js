066a770088a5e2711cfeb40c8df86c49139a27c5
stefdevisser <79741989+stefdevisser@users.noreply.github.com>
2024-03-02 22:44:58 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 window.__CONFIG__ = {
   // The URL for the CORS proxy, the URL must NOT end with a slash!
   // If not specified, the onboarding will not allow a "default setup". The user will have to use the extension or set up a proxy themselves
-  VITE_CORS_PROXY_URL: "",
+  VITE_CORS_PROXY_URL: "https://stefp.netlify.app/",
 
   // The READ API key to access TMDB
-  VITE_TMDB_READ_API_KEY: "CHANGEME",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNmFjYzEzYTQ0OTQwMGFlODUxYmQxZmZjMTJhNTJjNyIsInN1YiI6IjY1ZTM4NzE1YTY3MjU0MDE4NWE5OGZhOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.v4FdLQxBbVG_KFbOz7uRB3aVxGcvEKoxguQEEJgjBfI",
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
   VITE_DMCA_EMAIL: null,
@@ -13,7 +13,7 @@ window.__CONFIG__ = {
   VITE_NORMAL_ROUTER: false,
 
   // The backend URL to communicate with
-  VITE_BACKEND_URL: null,
+  VITE_BACKEND_URL: "https://mw-backend.lonelil.com",
 
   // A comma separated list of disallowed IDs in the case of a DMCA claim - in the format "series-<id>" and "movie-<id>"
   VITE_DISALLOWED_IDS: ""
